678e69256fa60b6fbf90fc56a0974a2c39266171
Jeronimo Luis <71951762+Luis-Enrique-Jeronimo@users.noreply.github.com>
2022-02-10 15:18:14 -0600

    Calculadora v1.2

--- Python/Calculadora.py
+++ Python/Calculadora.py
@@ -1,17 +1,17 @@
 import Inicial  #Funcion que contiene las opciones de la calculadora.
-import sys
 
-letras = ('S','s','SI','si','Si') #Diccionario para verificación de las letras que introducira el usuario
+def pregunta(): #Imprime el menú y pregunta si deseas realizar otra operación.
+    
+    Inicial.menu()
+    
+    letras = ('S','s','SI','si','Si') #Diccionario para verificación de las letras que introducira el usuario
 
+    opc = str(input("-----------¿Desea realizar otra operación?-----------\n\t : "))
+    
+    if (opc in letras):
+            Inicial.menu()
+            pregunta()
+    else:
+        print("Adios.!!")
 
-Inicial.menu()
-op = str(input("-----------¿Desea realizar otra operación?-----------\n :_"))
-
-#Si lo introducido en la variable es igual al contenido de la lista entonces se vuelve a iniciar el programa.
-for op in letras:
-    if (op == letras):
-        Inicial.menu()
-
-    elif(op != letras):
-        sys.exit()
-
+pregunta()
